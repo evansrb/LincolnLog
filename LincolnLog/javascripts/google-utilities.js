9eed9379b7348fa4f16bcc5496afe8508eab964c
Michael Scribellito <mscribellito@gmail.com>
2013-04-10 21:42:33 -0400

    update some JS

---
+++ LincolnLog/javascripts/google-utilities.js
@@ -0,0 +1,24 @@
+﻿function getLatLngFromName(name) {
+
+    var geocoder = 
+
+    geocoder.geocode({ 'address': "Springfield, IL" }, function (results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+
+            alert('x');
+
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+
+        } else {
+
+            alert("Geocode was not successful for the following reason: " + status);
+
+        }
+
+    });
+
+}
---
+++ LincolnLog/javascripts/script.js
@@ -0,0 +1,21 @@
+﻿function getLatLngFromName(name, callback) {
+
+    var place = { "address": name };
+
+    var result = false;
+
+    geocoder.geocode(place, function (results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+
+            result = results[0].geometry.location;
+
+            callback(map, pin, result);
+
+        }
+
+    });
+
+    return result;
+
+}
